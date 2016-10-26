89a69842b5279fb3cfa1ac3833402512ab3659e2
Pei Lian Liu <epsonavy@yahoo.com>
2016-10-26 00:08:27 -0700

    first commit

---
+++ TouchTracker/Line.swift
@@ -0,0 +1,15 @@
+//
+//  Line.swift
+//  TouchTracker
+//
+//  Created by Pei Liu on 2016/10/26.
+//  Copyright © 2016年 Pei Liu. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+
+struct Line {
+    var begin = CGPoint.zero
+    var end = CGPoint.zero
+}
